42b54b67dd5d19774ed5f4793ac30582c930f7f4
Siva Koneti <41994167+sivakoneti@users.noreply.github.com>
2021-03-05 17:13:44 +0530

    fix: footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,16 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
+    {/* <h2 className="taCenter">
       Follow us{' '}
       <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
+    </h2> */}
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://tentsingandikota.com/">tentsingandikota</a>.
         </span>
       </div>
     </footer>
